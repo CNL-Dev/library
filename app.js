7b5d84164391caba19906dc77716cf588ece21b0
Caleb Lugo <cnl1211999@gmail.com>
2024-01-10 15:33:01 -0800

    feat: add addBookToLibrary function, update docs

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const library  = [];
 
+// Book constructor
 function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
+}
+
+// Add a book to the library array
+function addBookToLibrary(book) {
+    library.push(book);
 }
